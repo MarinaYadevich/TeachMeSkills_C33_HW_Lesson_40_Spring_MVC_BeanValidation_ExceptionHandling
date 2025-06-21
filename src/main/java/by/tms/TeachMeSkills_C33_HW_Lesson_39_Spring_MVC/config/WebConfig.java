bb3420c3d576ece8e0a4f8c7c00fe54a80bcf05c
Marina Yadevich <marishka.kazakova.2012@mail.ru>
2025-06-21 17:19:54 +0300

    Add package Config.

--- src/main/java/by/tms/TeachMeSkills_C33_HW_Lesson_39_Spring_MVC/config/WebConfig.java
+++ src/main/java/by/tms/TeachMeSkills_C33_HW_Lesson_39_Spring_MVC/config/WebConfig.java
@@ -1,4 +1,4 @@
-package by.tms.TeachMeSkills_C33_HW_Lesson_39_Spring_MVC;
+package by.tms.TeachMeSkills_C33_HW_Lesson_39_Spring_MVC.config;
 
 import by.tms.TeachMeSkills_C33_HW_Lesson_39_Spring_MVC.interceptor.LoggingInterceptor;
 import org.springframework.context.annotation.Configuration;
